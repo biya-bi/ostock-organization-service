fa70e69f29a929923560cbc26d0b13026771f2d3
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-04-27 21:02:07 -0400

    Improve the startup script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [[ ! -f ${JWT_ISSUER_URI_FILE} ]]; then
+    echo "The file ${JWT_ISSUER_URI_FILE} does not exist"
+    exit 1
+fi
+
 export JWT_ISSUER_URI=`cat ${JWT_ISSUER_URI_FILE}`
 
 java org.springframework.boot.loader.launch.JarLauncher
