2628d46bc5d32f42d222ee445177434f1d025e84
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-07-25 00:46:17 -0400

    Reuse EnvFileReader for reading environment files

--- src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
+++ src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
@@ -1,14 +1,14 @@
 package com.optimagrowth.organization;
 
 import java.io.IOException;
+import java.util.Map;
 
+import org.rainbow.io.EnvFileReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
-import com.optimagrowth.organization.io.EnvReader;
 
 @SpringBootApplication
 @RefreshScope
@@ -17,9 +17,17 @@ import com.optimagrowth.organization.io.EnvReader;
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) throws IOException {
-		EnvReader.read();
-		
+		readAndSet();
+
 		SpringApplication.run(OrganizationServiceApplication.class, args);
+	}
+
+	private static void readAndSet() throws IOException {
+		EnvFileReader.readAndSet(
+				Map.of("JWT_ISSUER_URI_FILE", "spring.security.oauth2.resourceserver.jwt.issuerUri",
+						"DATASOURCE_URL_FILE", "spring.datasource.url",
+						"DATASOURCE_USERNAME_FILE", "spring.datasource.username",
+						"DATASOURCE_PASSWORD_FILE", "spring.datasource.password"));
 	}
 
 }
