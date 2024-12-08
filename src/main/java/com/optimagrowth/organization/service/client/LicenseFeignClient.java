9a780a80628bbee593e90533a5c9ad3bfe0b3f79
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2024-12-08 13:46:29 -0500

    Update licenses endpoint

--- src/main/java/com/optimagrowth/organization/service/client/LicenseFeignClient.java
+++ src/main/java/com/optimagrowth/organization/service/client/LicenseFeignClient.java
@@ -5,8 +5,8 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.optimagrowth.dto.LicenseDto;
 
@@ -17,6 +17,6 @@ import io.github.resilience4j.retry.annotation.Retry;
 @CircuitBreaker(name = "licenseFeignClient")
 @Retry(name = "licenseFeignClient")
 public interface LicenseFeignClient {
-    @GetMapping("/v1/license/{organizationId}")
+    @PostMapping("/v1/license/search/{organizationId}")
     ResponseEntity<CollectionModel<LicenseDto>> getLicenses(@PathVariable("organizationId") UUID organizationId);
 }
