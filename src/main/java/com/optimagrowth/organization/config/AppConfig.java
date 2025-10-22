ba9fe17f01c0ac5b4bcd36e320a26525833a8f9d
Paul Muriel Biya-Bi <paulmuriel.biyabi@gmail.com>
2025-10-21 22:39:36 -0400

    Use security configurations from web-oauth2

--- src/main/java/com/optimagrowth/organization/config/AppConfig.java
+++ src/main/java/com/optimagrowth/organization/config/AppConfig.java
@@ -1,11 +1,12 @@
 package com.optimagrowth.organization.config;
 
+import org.nguiland.web.security.config.SecurityConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.optimagrowth.config.CrossCuttingConcernsConfig;
 
 @Configuration
-@ComponentScan(basePackageClasses = { CrossCuttingConcernsConfig.class })
+@ComponentScan(basePackageClasses = { CrossCuttingConcernsConfig.class, SecurityConfig.class })
 class AppConfig {
 }
